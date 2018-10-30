16bd985e2985f19946157b3d4687d1a5780e2e88
manoja328 <manoja328@gmail.com>
2018-10-30 16:29:15 -0400

    add better loss plotter

--- plot_params.py
+++ plot_params.py
@@ -1,6 +1,8 @@
 import argparse
 import os
 import utils
+import seaborn as sns
+import numpy as np
 import matplotlib.pyplot as plt
 
 def parse_args():
@@ -11,17 +13,26 @@ def parse_args():
 
 if __name__ == '__main__':
     
-    args = parse_args()
-       
-    savefolder = args.save
-    
+    args = parse_args()      
+    savefolder = args.save    
     info_json = os.path.join(savefolder,'infos.json')
-    js = utils.parsejson(info_json)
-    
+    js = utils.parsejson(info_json)   
+    colors = iter('rbgkcm')
+    print (js.keys())
+    sns.set()
+    sns.set_context("paper")
+    plt.figure(figsize=(8,4))
     for key in js:
         L = len(js[key])
-        plt.plot(range(L),js[key],'o')
-        plt.plot(range(L),js[key],label=key)
-    plt.xlabel("Epochs")
-    plt.legend()
+        c = next(colors)
+        if 'loss' in key:
+            plt.subplot(1,2,1)
+            plt.ylabel('Loss')
+        else:
+            plt.subplot(1,2,2)
+            plt.ylabel('Accuracy')
+        plt.plot(range(L),js[key],'o',color=c)
+        plt.plot(range(L),js[key],label=key,color=c)
+        plt.xlabel("Epochs")
+        plt.legend()
     plt.show()
